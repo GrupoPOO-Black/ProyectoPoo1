e0ed4eb29e77939db836e6bee71ccc39ce8c693f
Kenneth <kfhv.24@gmail.com>
2017-09-15 15:12:01 -0600

    Creados metodos de guardado.

--- src/app/Main.java
+++ src/app/Main.java
@@ -8,35 +8,62 @@ import xml.*;
 
 @SuppressWarnings("unchecked")
 public class Main {
-
+	
+	static List<Estudiante> students = new ArrayList<Estudiante>();
+	static List<Sala> rooms = new ArrayList<Sala>();
+	static List<Reserva> reservations = new ArrayList<Reserva>();
 	
 
-	/**
-	 * 
-	 * 
-	 * Launch the application.
-	 * @throws Exception 
-	 */
-	
 	
 	public static void main(String[] args) throws Exception {
 		
-		List<Estudiante> sList = new ArrayList<Estudiante>();
 		
 		
-		sList.add(new Estudiante("Kenneth","2016","Ing. Computacion","kfhv.24@gmail.com",100,"89657436"));
-
-		XmlParser.write(sList, "Estudiantes.xml");
-		
+		students.add(new Estudiante("Kenneth","2016","Ing. Computacion","kfhv.24@gmail.com",100,"89657436"));
 		
 		
 		Sala sala = new Sala(5);
 	    
 	    
-	    Reserva reserva = new Reserva(sList.get(0), sala, new Hora());
+	    Reserva reserva = new Reserva(students.get(0), sala, new Hora());
 	    
 	    System.out.println(reserva.toString());
 		
 	}
 
+	void saveData() {
+		try {
+			XmlParser.write(students, "EstudiantesDB.xml");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		try {
+			XmlParser.write(rooms, "SalasDB.xml");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		try {
+			XmlParser.write(reservations, "ReservacionesDB.xml");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	void loadData() {
+		try {
+			students = (ArrayList<Estudiante>) XmlParser.read("EstudiantesDB.xml");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		try {
+			rooms = (ArrayList<Sala>) XmlParser.read("SalaDB.xml");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		try {
+			reservations = (ArrayList<Reserva>) XmlParser.read("ReservacionesDB.xml");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
