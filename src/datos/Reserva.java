47b154ecfc92b6d45c6d3eeb33e9d952a47c52c2
Kenneth <kfhv.24@gmail.com>
2017-09-17 18:19:21 -0600

    Cambios en la metodologia de Fecha

--- src/datos/Reserva.java
+++ src/datos/Reserva.java
@@ -7,14 +7,16 @@ public class Reserva {
 	private int reserveID;
 	private Estudiante student;
 	private Sala room;
+	private GregorianCalendar date;
 	private Hora hour;
 	private String subject;
 	
 	
-	public Reserva(Estudiante pStudent,Sala pRoom,Hora pHour,String pSubject){
+	public Reserva(Estudiante pStudent,Sala pRoom,GregorianCalendar pDate,Hora pHour,String pSubject){
 		reserveID = ++reservations;
 		student = pStudent;
 		room =  pRoom;
+		date = pDate;
 		hour = pHour;
 		subject = pSubject;
 	}
@@ -24,7 +26,8 @@ public class Reserva {
 		msg += "\nAsunto a tratar:\n" + subject;
 		msg += "\nEstudiante:\n" + student.toString();
 		msg += "Sala:\n" +  room.toString();
-		msg += "Hora:\n" + hour.toString();
+		msg += "Fecha:\n " + date.get(date.DAY_OF_MONTH) + "/" + date.get(date.MONTH) + "/" +  date.get(date.YEAR);
+		msg += "\nHora:\n" + hour.toString();
 		return msg;
 	}
 	
