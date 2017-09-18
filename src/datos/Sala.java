0ae287c535d2369ce825ec28107781c1b2752f84
Kenneth <kfhv.24@gmail.com>
2017-09-17 18:18:27 -0600

    Cambios en constructores e implementacion de Horario

--- src/datos/Sala.java
+++ src/datos/Sala.java
@@ -11,29 +11,36 @@ import java.util.ArrayList;
  */
 
 public class Sala {
+	
+	public static boolean INACTIVE = false;
+	public static boolean ACTIVE = false;
+	
 	private static int cSalas = 001;
 	private String id = "SAL-";
 	private int capacity;
 	private ArrayList<String> resources = new ArrayList<String>();
 	
-	private String status = "Inactive";
+	private boolean status = INACTIVE;
 	private float score = 100;
+	
+	private Horario schedule;
 	
 	public Sala(){
 	}
 	
-	
-	public Sala(int pcapacity){
-		id = id + cSalas;
-		cSalas++;
-		capacity = pcapacity;
-	}
-	
-	public Sala(int pcapacity, String pstatus){
+	public Sala(int pcapacity, boolean pstatus,Horario pSchedule){
 		id = id + cSalas;
 		cSalas++;
 		capacity = pcapacity;
 		status = pstatus;
+		schedule = pSchedule;
+	}
+	
+	public Sala(int pcapacity,Horario pSchedule){
+		id = id + cSalas;
+		cSalas++;
+		capacity = pcapacity;
+		schedule = pSchedule;
 	}
 	
 	public void addresource(String resource) {
@@ -52,16 +59,28 @@ public class Sala {
 		return capacity;
 	}
 
-	public String getStatus() {
+	public boolean getStatus() {
 		return status;
 	}
 
 	public float getScore() {
 		return score;
 	}
+	
+	public Horario getSchedule(){
+		return schedule;
+	}
+	
+	public void setSchedule(Horario pSchedule) {
+		schedule = pSchedule;
+	}
+	
+	public void switchStatus() {
+		status = !status;
+	}
 
 	public String toString(){
-		return(id + "\n Capacidad: " + capacity + "\n Estado: " + status + "\n");
+		return(id + "\n Capacidad: " + capacity + "\n Estado: " + status + "\n" + schedule.toString());
 	}
 	
 	
