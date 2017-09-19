e40b98fe1c6d74ee125c56cf077358b55b6bef73
Kenneth <kfhv.24@gmail.com>
2017-09-19 11:55:43 -0600

    Se implementa Serializable y se agregaron setters.

--- src/datos/Reserva.java
+++ src/datos/Reserva.java
@@ -1,26 +1,63 @@
 package datos;
 
+import java.io.Serializable;
 import java.util.*;
 
-public class Reserva {
+public class Reserva implements Serializable {
 	private static int reservations = 0;
 	private int reserveID;
 	private Estudiante student;
+	private int people;
 	private Sala room;
 	private GregorianCalendar date;
 	private Hora hour;
 	private String subject;
 	
 	
-	public Reserva(Estudiante pStudent,Sala pRoom,GregorianCalendar pDate,Hora pHour,String pSubject){
+	public Reserva(Estudiante pStudent,Sala pRoom,GregorianCalendar pDate,Hora pHour,String pSubject,int pPeople){
 		reserveID = ++reservations;
 		student = pStudent;
 		room =  pRoom;
 		date = pDate;
 		hour = pHour;
 		subject = pSubject;
+		people = pPeople;
 	}
 	
+	public int getReserveID() {
+		return reserveID;
+	}
+
+	public Estudiante getStudent() {
+		return student;
+	}
+
+	public Sala getRoom() {
+		return room;
+	}
+
+
+
+	public GregorianCalendar getDate() {
+		return date;
+	}
+
+
+
+	public Hora getHour() {
+		return hour;
+	}
+
+
+
+	public String getSubject() {
+		return subject;
+	}
+	
+	public int getPeople() {
+		return people;
+	}
+
 	public String toString() {
 		String msg = "Reserva:\nID: " + reserveID;
 		msg += "\nAsunto a tratar:\n" + subject;
