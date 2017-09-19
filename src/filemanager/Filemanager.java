f4338186bbee2eb3ed7a5381529a23ee4b88732d
Kenneth <kfhv.24@gmail.com>
2017-09-19 11:32:43 -0600

    Se crea clase filemanager para serializar clases.

---
+++ src/filemanager/Filemanager.java
@@ -0,0 +1,29 @@
+package filemanager;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public class Filemanager {
+	public static void save(Object pOb,String fileName) {
+		try (ObjectOutputStream oos =
+				new ObjectOutputStream(new FileOutputStream(fileName))) {
+
+			oos.writeObject(pOb);
+			System.out.println("Done");
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+	public static Object load(String fileName) throws IOException, ClassNotFoundException {
+		FileInputStream fin= new FileInputStream (fileName);
+		ObjectInputStream ois = new ObjectInputStream(fin);
+		Object tmp = ois.readObject(); 
+		fin.close();
+		return tmp;
+	}
+}
